697e801d59335c0bd89fab763790088d929ac414
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-01 17:39:57 +0100

    tasks individuate

---
+++ script.js
@@ -0,0 +1,4 @@
+/* devo inserire la funzionalità di scorrimento delle immagini */
+/* ---- collegare i bottoni nel js e dargli la funzionalità */
+/* ---- far scambiare il display none e il displey block in base alla immagine scorsa */
+/* ---- quindi implementare due classi con classe "display none e classe display block ( con bootstrap non dovrebbe servire)" */
