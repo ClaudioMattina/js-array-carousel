d75dc31a5f547d25e982aeea82549b6e9c5c3152
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-01 17:56:20 +0100

    preso elemento by class

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 /* ---- collegare i bottoni nel js e dargli la funzionalità */
 /* ---- far scambiare il display none e il displey block in base alla immagine scorsa */
 /* ---- quindi implementare due classi con classe "display none e classe display block ( con bootstrap non dovrebbe servire)" */
+
+
+const mainImg = document.getElementsByClassName("my-img-container");
+console.log(mainImg);
